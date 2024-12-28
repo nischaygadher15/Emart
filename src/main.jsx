650ad4b4ff4eedeb8601dff5653604bd93d957ce
nischay gadher <nischaygadher15@gmail.com>
2024-12-29 00:31:34 +0530

    Working on Drawer3

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router";
 import myRouter from "./Router/Router.jsx";
 import { Provider } from "react-redux";
 import Store from "./Redux/Store.js";
-import "./Index.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
