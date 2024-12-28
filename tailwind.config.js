781e4cf66f969be182f7839e6f743ada9ee2c2ce
nischay gadher <nischaygadher15@gmail.com>
2024-12-29 00:38:28 +0530

    Working on deploy error

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
-  plugins: [flowbite.plugin()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  plugins: [require("flowbite/plugin")],
 };
