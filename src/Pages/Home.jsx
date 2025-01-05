fb4737b16930aca5e22facd8cc4520f50f36a919
nischay gadher <nischaygadher15@gmail.com>
2025-01-05 13:57:23 +0530

    Working on filter drawer..

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Badge, Carousel } from "flowbite-react";
+import { Badge, Carousel, Drawer } from "flowbite-react";
 import { Pagination } from "flowbite-react";
 import React, { useEffect, useRef, useState } from "react";
 import c1 from "../assets/Carousel_Images/c_banner1.png";
@@ -9,11 +9,12 @@ import c3 from "../assets/Carousel_Images/c_banner3.png";
 import c4 from "../assets/Carousel_Images/c_banner4.png";
 import { RiArrowLeftWideFill, RiArrowRightWideFill } from "react-icons/ri";
 import { Accordion } from "flowbite-react";
-import { FaDotCircle, FaStar } from "react-icons/fa";
+import { FaDotCircle, FaFilter, FaStar } from "react-icons/fa";
 import ReactSlider from "react-slider";
 import "../App.css";
 import ProductList from "../Data/ProductsList";
 import { IoIosArrowBack } from "react-icons/io";
+import Filters from "../components/Filters";
 
 const Home = () => {
   // <========================== Variable Declaration ==========================>
@@ -57,6 +58,10 @@ const Home = () => {
   let hadleScrrenSize = () => {
     setScrernWidth(window.screen.width);
   };
+
+  // <========================== React Pagination ==========================>
+  const [filterDrawer, setFilterDrawer] = useState(false);
+  const handleClose = () => setFilterDrawer(false);
 
   return (
     <div className="relative px-4 w-full sm:min-w-[640px] md:min-w-[798px] lg:min-w-[1024px]">
@@ -269,192 +274,40 @@ const Home = () => {
         </div>
       </div>
 
-      <p className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center py-10">
+      <p className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center mt-10 mb-5 ">
         Our Products
       </p>
 
+      {/* Sort BY */}
+      <div className="h-14 mb-5 flex justify-between items-center">
+        <div>
+          <button
+            className="md:hidden text-lg flex justify-center items-center gap-2 p-2 rounded bg-[#292560]
+        text-white font-semibold hover:bg-[#FDB03D]"
+            onClick={() => setFilterDrawer(true)}
+          >
+            Filters
+            <FaFilter />
+          </button>
+        </div>
+
+        <form>
+          <label htmlFor="sortby" className="text-lg font-semibold me-2">
+            Sort by
+          </label>
+          <select className="p-1 rounded" id="sortby">
+            <option value="Select">Select</option>
+            <option value="Select">Price: Low to High</option>
+            <option value="Select">Price: High to Low</option>
+          </select>
+        </form>
+      </div>
+
+      {/* Product Box */}
       <div className="max-w-full flex gap-4" ref={productBox}>
         {/* Filters */}
         <div className="hidden md:block md:w-1/4">
-          <div
-            className="w-full h-auto shadow shadow-gray-500 rounded"
-            style={{
-              border: "1px solid rgba(180, 180, 180)",
-            }}
-          >
-            {/* Title */}
-            <div className="p-3 lg:p-5">
-              <p className="text-2xl font-semibold text-[rgba(0,0,0,0.8)]">
-                Filters
-              </p>
-            </div>
-            <hr
-              style={{
-                border: "1px solid rgba(180, 180, 180)",
-              }}
-            />
-
-            {/* Select Category */}
-            <div className="p-3 lg:p-5">
-              <p className="text-lg font-semibold text-[rgba(0,0,0,0.8)] mb-3">
-                Categories
-              </p>
-              <select
-                name="category"
-                className="w-full p-2 bg-[#F3F4F6] font-semibold text-[rgba(0,0,0,0.8)] rounded"
-              >
-                <option value="select">Select Category</option>
-                <option value="select">Category</option>
-                <option value="select">Category</option>
-                <option value="select">Category</option>
-                <option value="select">Category</option>
-              </select>
-            </div>
-            <hr
-              style={{
-                border: "1px solid rgba(180, 180, 180)",
-              }}
-            />
-
-            {/* Price Range */}
-            <div className="p-3 lg:p-5">
-              <p className="text-lg font-semibold text-[rgba(0,0,0,0.8)] mb-3">
-                Price Range
-              </p>
-              <p className="font-semibold text-[rgba(0,0,0,0.8)]">
-                &#8377; {rangeVal[0]} - &#8377; {rangeVal[1]}
-              </p>
-
-              {/* Range Slider */}
-              <div className="py-9">
-                <ReactSlider
-                  className="horizontal-slider"
-                  thumbClassName=""
-                  trackClassName={`trackClass`}
-                  defaultValue={[11.111, 77.778]}
-                  ariaLabelledby={["first-slider-label", "second-slider-label"]}
-                  ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
-                  renderThumb={(props, state) => {
-                    const { key, ...restProps } = props;
-                    const prop = { ...restProps };
-                    return (
-                      <div
-                        key={`reactSlider${key}`}
-                        {...prop}
-                        className="outline-none"
-                      >
-                        <p
-                          className="absolute w-5 h-5 flex justify-center items-center 
-                    rounded-full bg-blue-500 text-white"
-                          style={{ top: "-8px", left: "-8px" }}
-                        ></p>
-                      </div>
-                    );
-                  }}
-                  pearling
-                  minDistance={15}
-                  withTracks={true}
-                  step={0.45}
-                  // value={[0, 4500]}
-                  onChange={(state) => {
-                    let minVal = Math.round(state[0] * 45);
-                    let maxVal = Math.round(state[1] * 45);
-                    setRangeVal([minVal, maxVal]);
-                  }}
-                />
-              </div>
-            </div>
-            <hr
-              style={{
-                border: "1px solid rgba(180, 180, 180)",
-              }}
-            />
-
-            {/* Discount */}
-            <div className="p-3 lg:p-5">
-              <Accordion>
-                <Accordion.Panel>
-                  <Accordion.Title className="p-3">Discount</Accordion.Title>
-                  <Accordion.Content>
-                    <ul>
-                      <li className="flex items-center">
-                        <input type="radio" className="me-2" />
-                        50% or more
-                      </li>
-                    </ul>
-                  </Accordion.Content>
-                </Accordion.Panel>
-              </Accordion>
-            </div>
-            <hr
-              style={{
-                border: "1px solid rgba(180, 180, 180)",
-              }}
-            />
-
-            {/* Electronics */}
-            <div className="p-3 lg:p-5">
-              <Accordion>
-                <Accordion.Panel>
-                  <Accordion.Title className="p-3">Electronics</Accordion.Title>
-                  <Accordion.Content>
-                    <ul>
-                      <li className="flex items-center">
-                        <input type="checkbox" className="me-2" />
-                        Accesorries
-                      </li>
-                    </ul>
-                  </Accordion.Content>
-                </Accordion.Panel>
-              </Accordion>
-            </div>
-            <hr
-              style={{
-                border: "1px solid rgba(180, 180, 180)",
-              }}
-            />
-
-            {/* Sizes */}
-            <div className="p-3 lg:p-5">
-              <Accordion>
-                <Accordion.Panel>
-                  <Accordion.Title className="p-3">Size</Accordion.Title>
-                  <Accordion.Content>
-                    <ul>
-                      <li className="flex items-center">
-                        <input type="radio" className="me-2" />
-                        X-Large
-                      </li>
-                    </ul>
-                  </Accordion.Content>
-                </Accordion.Panel>
-              </Accordion>
-            </div>
-            <hr
-              style={{
-                border: "1px solid rgba(180, 180, 180)",
-              }}
-            />
-
-            {/* Customer Rating */}
-            <div className="p-3 lg:p-5">
-              <Accordion>
-                <Accordion.Panel>
-                  <Accordion.Title className="p-3">
-                    Customer Rating
-                  </Accordion.Title>
-                  <Accordion.Content>
-                    <ul>
-                      <li className="flex items-center gap-2">
-                        <input type="radio" />4
-                        <FaStar className="text-yellow-300" />& Above
-                      </li>
-                    </ul>
-                  </Accordion.Content>
-                </Accordion.Panel>
-              </Accordion>
-            </div>
-          </div>
+          <Filters range={{ rangeVal, setRangeVal }} />
         </div>
 
         {/* Products Grid */}
@@ -525,6 +378,12 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      {/* Filter Drawer */}
+      <Drawer open={filterDrawer} onClose={handleClose} position="left">
+        <Drawer.Header title="Drawer" />
+        <p>Hello</p>
+      </Drawer>
     </div>
   );
 };
--- src/Pages/doem.jsx
+++ src/Pages/doem.jsx
@@ -1,45 +1,171 @@
-<div className="w-3/4">
-  {/* Grid */}
-  <div className="w-full h-auto grid grid-cols-3 grid-rows-4 gap-4">
-    {currentItems.map((p, inx) => {
-      return (
-        <div
-          className="p-5 rounded shadow shadow-gray-500"
-          key={`ProductGrid-${inx}`}
-          style={{
-            border: "1px solid rgba(180, 180, 180)",
-          }}
-        >
-          <img
-            src={p.image[0]}
-            alt={`ProductGrid-${inx}`}
-            className="min-h-60 max-h-60 w-auto mx-auto"
-          />
-          <div className="w-full h-32 flex flex-col justify-between">
-            <p className="w-full font-semibold">
-              {`${p.title.substring(0, 50)}...`}
-            </p>
-            <p className="text-blue-500 font-semibold">${p.price}</p>
-            <div>
-              <button
-                className="w-32 p-2 rounded bg-[#292560] text-white font-semibold
-                     hover:bg-[#FDB03D]"
-              >
-                ADD TO CART
-              </button>
-            </div>
-          </div>
-        </div>
-      );
-    })}
+<div
+  className="w-full h-auto shadow shadow-gray-500 rounded"
+  style={{
+    border: "1px solid rgba(180, 180, 180)",
+  }}
+>
+  {/* Title */}
+  <div className="p-3 lg:p-5">
+    <p className="text-2xl font-semibold text-[rgba(0,0,0,0.8)]">Filters</p>
   </div>
+  <hr
+    style={{
+      border: "1px solid rgba(180, 180, 180)",
+    }}
+  />
 
-  {/* React Pagination */}
-  <div className="flex overflow-x-auto sm:justify-center my-7">
-    <Pagination
-      currentPage={currentPage}
-      totalPages={product.length / itemsPerPage}
-      onPageChange={onPageChange}
-    />
+  {/* Select Category */}
+  <div className="p-3 lg:p-5">
+    <p className="text-lg font-semibold text-[rgba(0,0,0,0.8)] mb-3">
+      Categories
+    </p>
+    <select
+      name="category"
+      className="w-full p-2 bg-[#F3F4F6] font-semibold text-[rgba(0,0,0,0.8)] rounded"
+    >
+      <option value="select">Select Category</option>
+      <option value="select">Category</option>
+      <option value="select">Category</option>
+      <option value="select">Category</option>
+      <option value="select">Category</option>
+    </select>
+  </div>
+  <hr
+    style={{
+      border: "1px solid rgba(180, 180, 180)",
+    }}
+  />
+
+  {/* Price Range */}
+  <div className="p-3 lg:p-5">
+    <p className="text-lg font-semibold text-[rgba(0,0,0,0.8)] mb-3">
+      Price Range
+    </p>
+    <p className="font-semibold text-[rgba(0,0,0,0.8)]">
+      &#8377; {rangeVal[0]} - &#8377; {rangeVal[1]}
+    </p>
+
+    {/* Range Slider */}
+    <div className="py-9">
+      <ReactSlider
+        className="horizontal-slider"
+        thumbClassName=""
+        trackClassName={`trackClass`}
+        defaultValue={[11.111, 77.778]}
+        ariaLabelledby={["first-slider-label", "second-slider-label"]}
+        ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
+        renderThumb={(props, state) => {
+          const { key, ...restProps } = props;
+          const prop = { ...restProps };
+          return (
+            <div key={`reactSlider${key}`} {...prop} className="outline-none">
+              <p
+                className="absolute w-5 h-5 flex justify-center items-center 
+                    rounded-full bg-blue-500 text-white"
+                style={{ top: "-8px", left: "-8px" }}
+              ></p>
+            </div>
+          );
+        }}
+        pearling
+        minDistance={15}
+        withTracks={true}
+        step={0.45}
+        // value={[0, 4500]}
+        onChange={(state) => {
+          let minVal = Math.round(state[0] * 45);
+          let maxVal = Math.round(state[1] * 45);
+          setRangeVal([minVal, maxVal]);
+        }}
+      />
+    </div>
+  </div>
+  <hr
+    style={{
+      border: "1px solid rgba(180, 180, 180)",
+    }}
+  />
+
+  {/* Discount */}
+  <div className="p-3 lg:p-5">
+    <Accordion>
+      <Accordion.Panel>
+        <Accordion.Title className="p-3">Discount</Accordion.Title>
+        <Accordion.Content>
+          <ul>
+            <li className="flex items-center">
+              <input type="radio" className="me-2" />
+              50% or more
+            </li>
+          </ul>
+        </Accordion.Content>
+      </Accordion.Panel>
+    </Accordion>
+  </div>
+  <hr
+    style={{
+      border: "1px solid rgba(180, 180, 180)",
+    }}
+  />
+
+  {/* Electronics */}
+  <div className="p-3 lg:p-5">
+    <Accordion>
+      <Accordion.Panel>
+        <Accordion.Title className="p-3">Electronics</Accordion.Title>
+        <Accordion.Content>
+          <ul>
+            <li className="flex items-center">
+              <input type="checkbox" className="me-2" />
+              Accesorries
+            </li>
+          </ul>
+        </Accordion.Content>
+      </Accordion.Panel>
+    </Accordion>
+  </div>
+  <hr
+    style={{
+      border: "1px solid rgba(180, 180, 180)",
+    }}
+  />
+
+  {/* Sizes */}
+  <div className="p-3 lg:p-5">
+    <Accordion>
+      <Accordion.Panel>
+        <Accordion.Title className="p-3">Size</Accordion.Title>
+        <Accordion.Content>
+          <ul>
+            <li className="flex items-center">
+              <input type="radio" className="me-2" />
+              X-Large
+            </li>
+          </ul>
+        </Accordion.Content>
+      </Accordion.Panel>
+    </Accordion>
+  </div>
+  <hr
+    style={{
+      border: "1px solid rgba(180, 180, 180)",
+    }}
+  />
+
+  {/* Customer Rating */}
+  <div className="p-3 lg:p-5">
+    <Accordion>
+      <Accordion.Panel>
+        <Accordion.Title className="p-3">Customer Rating</Accordion.Title>
+        <Accordion.Content>
+          <ul>
+            <li className="flex items-center gap-2">
+              <input type="radio" />4
+              <FaStar className="text-yellow-300" />& Above
+            </li>
+          </ul>
+        </Accordion.Content>
+      </Accordion.Panel>
+    </Accordion>
   </div>
 </div>;
